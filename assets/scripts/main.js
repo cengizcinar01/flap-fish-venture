d436fd3647371102506a3d607bdbba6300cedea0
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-02-28 23:40:59 +0100

    Added Game class and render method in main.js
    The class has a constructor that initializes the canvas and context, as well as its width and height. A render method is also added to the class, which draws a rectangle on the canvas. Additionally, an instance of the Game class is created when the window loads.

---
+++ assets/scripts/main.js
@@ -0,0 +1,20 @@
+class Game {
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.ctx = context;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+    }
+    render() {
+        this.ctx.fillRect(100, 100, 50, 150);
+    }
+}
+
+window.addEventListener('load', function () {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 720;
+    canvas.height = 720;
+
+    const game = new Game(canvas, ctx);
+});
