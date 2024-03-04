acc1dc12b536bb67e5f80fdfe7bb19884573d5c3
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-04 23:56:08 +0100

    fix: adjust image positioning in background.js
    Adjusted the positioning of the image in the `draw()` method within the `Background` class in `background.js`. This fix ensures the image is correctly positioned and displayed, improving the overall visual experience.

--- assets/scripts/background.js
+++ assets/scripts/background.js
@@ -14,7 +14,10 @@ class Background {
     }
     draw() {
         this.game.ctx.drawImage(this.image, this.x, 0, this.scaledWidth, this.scaledHeight);
-        this.game.ctx.drawImage(this.image, this.x + this.scaledWidth - 2, 0, this.scaledWidth, this.scaledHeight);
+        this.game.ctx.drawImage(this.image, this.x + this.scaledWidth - 1, 0, this.scaledWidth, this.scaledHeight);
+        if (this.game.canvas.width >= this.scaledWidth) {
+            this.game.ctx.drawImage(this.image, this.x + this.scaledWidth * 2 - 2, 0, this.scaledWidth, this.scaledHeight);
+        }
     }
     resize() {
         this.scaledWidth = this.width * this.game.ratio;
