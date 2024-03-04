19d10a902472c15682f495198ac2d74dc3a47cc6
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-04 23:42:09 +0100

    fix: constructor and added resize method

--- assets/scripts/background.js
+++ assets/scripts/background.js
@@ -1,6 +1,6 @@
 class Background {
     constructor(game) {
-        this, (game = game);
+        this.game = game;
         this.image = document.getElementById('background');
         this.width = 2400;
         this.height = this.game.baseHeight;
@@ -9,5 +9,8 @@ class Background {
     update() {}
     draw() {
         this.game.ctx.drawImage(this.image, this.x, 0);
+    }
+    resize() {
+        this.x = 0;
     }
 }
