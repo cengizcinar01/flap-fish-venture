901130f9d727455448d5739787903622ac79c090
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-04 23:37:40 +0100

    Added new Background class in background.js

---
+++ assets/scripts/background.js
@@ -0,0 +1,13 @@
+class Background {
+    constructor(game) {
+        this, (game = game);
+        this.image = document.getElementById('background');
+        this.width = 2400;
+        this.height = this.game.baseHeight;
+        this.x;
+    }
+    update() {}
+    draw() {
+        this.game.ctx.drawImage(this.image, this.x, 0);
+    }
+}
