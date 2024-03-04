dd5c6a461d28814808f912a3260bde5834f5158a
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-04 23:26:18 +0100

    Added flapSpeed to Player class
    This property is used to control the speed at which the player 'flaps'. The value of 'flapSpeed' is set in the 'init' method and used in the 'flap' method.

--- assets/scripts/player.js
+++ assets/scripts/player.js
@@ -8,6 +8,7 @@ class Player {
         this.width;
         this.height;
         this.speedY;
+        this.flapSpeed;
     }
     draw() {
         this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -26,6 +27,7 @@ class Player {
         this.height = this.spriteHeight * this.game.ratio;
         this.y = this.game.height * 0.5 - this.height * 0.5;
         this.speedY = -8 * this.game.ratio;
+        this.flapSpeed = 5 * this.game.ratio;
     }
     isTouchingTop() {
         return this.y <= 0;
@@ -35,7 +37,7 @@ class Player {
     }
     flap() {
         if (!this.isTouchingTop()) {
-            this.speedY = -5;
+            this.speedY = -this.flapSpeed;
         }
     }
 }
