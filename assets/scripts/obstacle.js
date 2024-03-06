03bcee0ee955d77cc0fb0be4c59ea8a711684e4d
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-06 22:10:47 +0100

    Added update method to Obstacle class

--- assets/scripts/obstacle.js
+++ assets/scripts/obstacle.js
@@ -3,7 +3,12 @@ class Obstacle {
         this.game = game;
         this.spriteWidth = 120;
         this.spriteHeight = 120;
+        this.scaledWidth = this.spriteWidth * this.game.ratio;
+        this.scaledHeight = this.spriteHeight * this.game.ratio;
         thus.x = x;
         this.y = this.game.height * 0.5 - this.height;
+    }
+    update() {
+        this.x -= this.game.speed;
     }
 }
