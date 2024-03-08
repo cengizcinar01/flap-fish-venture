4629a35145d8e012817d831204404b13a63343b2
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-08 18:56:42 +0100

    Game over condition added to obstacle.js

--- assets/scripts/obstacle.js
+++ assets/scripts/obstacle.js
@@ -20,6 +20,7 @@ class Obstacle {
             this.markedForDeletion = true;
             this.game.obstacles = this.game.obstacles.filter((obstacle) => !obstacle.markedForDeletion);
             console.log(this.game.obstacles.length);
+            if (this.game.obstacles.length <= 0) this.game.gameOver = true;
         }
     }
     draw() {
