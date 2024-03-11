ea0afbf62e5a720ee89830ee9f0d979694a98aab
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-11 23:47:42 +0100

    Add dynamic game over messages
    This commit introduces dynamic game over messages based on the player's performance.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,6 +15,8 @@ class Game {
         this.score;
         this.gameOver;
         this.timer;
+        this.message1;
+        this.message2;
 
         this.resize(window.innerWidth, window.innerHeight);
 
@@ -95,9 +97,19 @@ class Game {
         this.ctx.textAlign = 'left';
         this.ctx.fillText('Timer: ' + this.formatTimer(), 10, 30);
         if (this.gameOver) {
+            if (this.player.collided) {
+                this.message1 = 'Getting rusty?';
+                this.message2 = 'Collision time ' + this.formatTimer() + ' seconds!';
+            } else if (this.obstacles.length <= 0) {
+                this.message1 = 'Nailed it!';
+                this.message2 = 'Can you do it faster than ' + this.formatTimer() + ' seconds?';
+            }
             this.ctx.textAlign = 'center';
             this.ctx.font = '30px Bungee';
-            this.ctx.fillText('GAME OVER', this.width * 0.5, this.height * 0.5);
+            this.ctx.fillText(this.message1, this.width * 0.5, this.height * 0.5 - 40);
+            this.ctx.font = '15px Bungee';
+            this.ctx.fillText(this.message2, this.width * 0.5, this.height * 0.5 - 20);
+            this.ctx.fillText("Press 'R' to try again!", this.width * 0.5, this.height * 0.5);
         }
         this.ctx.restore();
     }
