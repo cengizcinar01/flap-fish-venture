ec2b8400f1210270ed9b78f5b7d13cc482c9979a
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-13 23:21:42 +0100

    Add color change to energy bar based on player's energy level

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -116,6 +116,8 @@ class Game {
             this.ctx.fillText(this.message2, this.width * 0.5, this.height * 0.5 - 20);
             this.ctx.fillText("Press 'R' to try again!", this.width * 0.5, this.height * 0.5);
         }
+        if (this.player.energy <= 20) this.ctx.fillStyle = 'blue';
+        else if (this.player.energy >= this.player.maxEnergy) this.ctx.fillStyle = 'orangered';
         for (let i = 0; i < this.player.energy; i++) {
             this.ctx.fillRect(10, this.height - 10 - 2 * i, 15, 2);
         }
