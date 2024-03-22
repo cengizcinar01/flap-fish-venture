f9da97a0789a57f3a4394f61bfe2116757b87c28
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-22 23:59:15 +0100

    Added new audio control for sound effect

--- assets/scripts/audio.js
+++ assets/scripts/audio.js
@@ -1,6 +1,7 @@
 class AudioControl {
     constructor() {
         this.charge = document.getElementById('charge');
+        this.flap1 = document.getElementById('flap1');
     }
     play(sound) {
         sound.currentTime = 0;
