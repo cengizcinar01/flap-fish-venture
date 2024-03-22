c52c746b39788aee380734344de75add73c7b23b
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-03-22 23:50:50 +0100

    feat: Added play method to AudioControl class

--- assets/scripts/audio.js
+++ assets/scripts/audio.js
@@ -2,4 +2,8 @@ class AudioControl {
     constructor() {
         this.charge = document.getElementById('charge');
     }
+    play(sound) {
+        sound.currentTime = 0;
+        sound.play();
+    }
 }
